9d512923247c3826d02c875b2d98926cac4fc6ab
shegeek <shegeek-dev@comcast.net>
2010-12-08 15:11:02 -0800

    starfield, options, destructor done

--- xtrascreenhacks-0.6/hacks/glx/craters.c
+++ xtrascreenhacks-0.6/hacks/glx/craters.c
@@ -42,9 +42,8 @@
 
 
 #define DEF_COUNT       "20"
-#define DEF_SIZE        "100"  /* needed? */
-#define DEF_STEP        "200"  /* needed? */
-#define DEF_FOG         "False"
+#define DEF_STEP        "10"
+#define DEF_WAITTIME    "10"
 
 
 
@@ -68,11 +67,11 @@ typedef struct
   GLuint stardlist;
 
   int timer;
-  int ctimer;
+  int wtimer;
 
   eraser_state *eraser;
   bool erasing;
-  ejemitter * eruption;
+  ejemitter *eruption;
   bool erupting;
   int eruptframes;
   double eruptincrement;
@@ -82,30 +81,25 @@ typedef struct
 static crater_configuration *lps = NULL;
 
 static int count;
-/* static int size; */
 static int step;
-static int do_fog;
+static int waittime;
 
 static XrmOptionDescRec opts[] = {
   {"-count", ".count", XrmoptionSepArg, 0},
-/*   {"-size", ".size", XrmoptionSepArg, 0}, */
   {"-step", ".step", XrmoptionSepArg, 0},
-  {"-fog", ".fog", XrmoptionNoArg, "True"},
-  {"+fog", ".fog", XrmoptionNoArg, "False"},
+  {"-waittime", ".waittime", XrmoptionSepArg, 0},
 };
 
 static argtype vars[] = {
   {&count, "count", "Count", DEF_COUNT, t_Int},
-/*   {&size, "size", "Size", DEF_SIZE, t_Int}, */
   {&step, "step", "Step", DEF_STEP, t_Int},
-  {&do_fog, "fog", "Fog", DEF_FOG, t_Bool},
+  {&waittime, "waittime", "waittime", DEF_WAITTIME, t_Int},
 };
 
 static OptionStruct desc[] = {
   {"-count", "maximum number of craters to form before clearing the screen"},
-/*   {"-size", "relative size of the craters on the screen"}, */
-  {"-step", "how far the ground should rotate in one frame"},
-  /* put fog here? */
+  {"-step", "how far the ejecta should fly in one frame"},
+  {"-waittime", "how long to wait between each crater"},
 };
 
 ENTRYPOINT ModeSpecOpt craters_opts =
@@ -134,52 +128,45 @@ handleGLerrors (char *guiltyfunction)
     }
 }
 
-static void errorCallback(GLenum errorCode)
+static void
+errorCallback (GLenum errorCode)
 {
-   const GLubyte *estring;
+  const GLubyte *estring;
 
-   estring = gluErrorString(errorCode);
-   fprintf(stderr, "Quadric Error: %s\n", estring);
-   exit(0);
+  estring = gluErrorString (errorCode);
+  fprintf (stderr, "Quadric Error: %s\n", estring);
+  exit (0);
 }
 
-/* works, but doesn't look good yet
- * parameters need to be reconciled between stars and ejecta,
- * stars need to be too close to be seen and are too large, some are square
- */
-GLuint make_starfield(double distance)
+
+GLuint
+make_starfield (double distance)
 {
+  GLfloat xpos, ypos;
+  int itor, numstars = 80;
   GLuint stf;
-  GLfloat starglow[] = {1., 1., 1., 1.};
-  GLfloat normal[] = {0., 0., 0., 1.};
-/*   GLdouble stdistance = distance - 10.; */
-  GLdouble stdistance = -50.;
-  stf = glGenLists(1);
-  glNewList(stf, GL_COMPILE);
+  GLfloat starglow[] = { 1., 1., 1., 1. };
+  GLfloat normal[] = { 0., 0., 0., 1. };
+  GLdouble stdistance = -60.;
+  stf = glGenLists (1);
+  glNewList (stf, GL_COMPILE);
 
-    glMaterialfv(GL_FRONT, GL_EMISSION, starglow);
-    glBegin(GL_POINTS);
-    glVertex3f(0., 3., stdistance);
-    glVertex3f(2.5, 3.3, stdistance);
-    glVertex3f(-1.7, 0.8, stdistance);
-    glVertex3f(-1.8, 2.5, stdistance);
-    glVertex3f(0.7, 6.6, stdistance);
-    glVertex3f(-2.0, 5.5, stdistance);
-    glVertex3f(3.1, 1.9, stdistance);
-    glVertex3f(-2.6, 2.1, stdistance);
-    glVertex3f(1.1, 1.1, stdistance);
-    glVertex3f(1.7, 3.3, stdistance);
-/*     glVertex3f(, , stdistance); */
-    glEnd();
-/*     glBegin(GL_TRIANGLES); */
-/*     glVertex3f(0., 0., stdistance); */
-/*     glVertex3f(10., 0., stdistance); */
-/*     glVertex3f(10., 10., stdistance); */
-/*     glEnd(); */
-    glMaterialfv(GL_FRONT, GL_EMISSION, normal);
-
-    glEndList();
-    return stf;
+  glMaterialfv (GL_FRONT, GL_EMISSION, starglow);
+  glPointSize (2.0);
+   glBegin (GL_POINTS);
+  for (itor = 0; itor < numstars; itor++)
+  {
+    xpos = (float)(random() % 400) / 10. - 20.;
+    ypos = (float)(random() % 190) / 10. - 4.;
+  glVertex3f (xpos, ypos, stdistance);
+  }
+    glVertex3f(0., 1., -0.5);
+  glEnd ();
+  glMaterialfv (GL_FRONT, GL_EMISSION, normal);
+  glPointSize (10.0);
+ 
+  glEndList ();
+  return stf;
 }
 
 
@@ -234,10 +221,10 @@ init_craters (ModeInfo * mi)
   GLfloat amb[4] = { 0.2, 0.2, 0.2, 1.0 };
   GLfloat dif[4] = { 1.0, 1.0, 1.0, 1.0 };
   GLfloat spc[4] = { 1.0, 1.0, 1.0, 1.0 };
-  GLfloat fogColor[4] = { 0., 0., 0., 1. };
   GLfloat pointdistparams[3];
-  GLUquadricObj * groundquad;
+  GLUquadricObj *groundquad;
   crater_configuration *lp;
+  double tstep;
   int loop;
 
   if (!lps)
@@ -255,7 +242,6 @@ init_craters (ModeInfo * mi)
   lp = &lps[MI_SCREEN (mi)];
   lp->glx_context = init_GL (mi);
 
-  lp->fogDepth = -350.0;
   lp->groundRadius = 50.0;
   lp->groundlevel = -6.;
 
@@ -263,53 +249,43 @@ init_craters (ModeInfo * mi)
   lp->numcraters = ((count > 0 && count <= MAX_CRATERS) ? count : 20);
   lp->countcraters = 0;
   lp->craters = calloc (lp->numcraters, sizeof (crater *));
-  for (loop = 0; loop < lp->numcraters; loop++) lp->craters[loop] = NULL;
+  for (loop = 0; loop < lp->numcraters; loop++)
+    lp->craters[loop] = NULL;
 
-  lp->ctimer = lp->timer = 10;
+  lp->wtimer = lp->timer = ((waittime > 0 && waittime <= 200) ? waittime : 10);
   lp->erasing = false;
-  lp->eruption = init_ejemitter(5000, 0.01, 25.);
+  tstep = ((step > 0 && step <= 20) ? ((float)step / 1000.) : 0.01);
+  lp->eruption = init_ejemitter (5000, tstep, 25.);
   lp->erupting = false;
 
-  groundquad = gluNewQuadric();
-  gluQuadricCallback(groundquad, GLU_ERROR, errorCallback);
-  gluQuadricDrawStyle(groundquad, GLU_FILL); 
-  gluQuadricNormals(groundquad, GLU_SMOOTH);
-  lp->craterdlist = glGenLists(1);
-  glNewList(lp->craterdlist, GL_COMPILE);
-  drawcrater4();
-  glEndList();
-  lp->grounddlist = glGenLists(1);
-  glNewList(lp->grounddlist, GL_COMPILE);
-  gluDisk(groundquad, 0., lp->groundRadius, 45, 15);
-  glEndList();
-  gluDeleteQuadric(groundquad);
-
-  lp->stardlist = make_starfield(lp->groundRadius);
-  
-
-  if (do_fog)
-    {
-      glEnable (GL_FOG);
-      glFogi (GL_FOG_MODE, GL_EXP2);
-      glFogf (GL_FOG_DENSITY, 0.004);
-      glHint (GL_FOG_HINT, GL_NICEST);
-      glFogf (GL_FOG_START, lp->fogDepth * 0.2);
-      glFogf (GL_FOG_END, lp->fogDepth);
-      glFogfv (GL_FOG_COLOR, fogColor);
-      glHint (GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    }
+  groundquad = gluNewQuadric ();
+  gluQuadricCallback (groundquad, GLU_ERROR, errorCallback);
+  gluQuadricDrawStyle (groundquad, GLU_FILL);
+  gluQuadricNormals (groundquad, GLU_SMOOTH);
+  lp->craterdlist = glGenLists (1);
+  glNewList (lp->craterdlist, GL_COMPILE);
+  drawcrater4 ();
+  glEndList ();
+  lp->grounddlist = glGenLists (1);
+  glNewList (lp->grounddlist, GL_COMPILE);
+  gluDisk (groundquad, 0., lp->groundRadius, 45, 15);
+  glEndList ();
+  gluDeleteQuadric (groundquad);
 
   glShadeModel (GL_SMOOTH);
-  glEnable (GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE); 
-    glCullFace(GL_BACK); 
+  glEnable (GL_CULL_FACE);
+  glCullFace (GL_BACK);
   glEnable (GL_POINT_SMOOTH);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable (GL_DEPTH_TEST);
   pointdistparams[0] = pointdistparams[2] = 0.0;
   pointdistparams[1] = 0.3;
   glPointParameterfv (GL_POINT_DISTANCE_ATTENUATION, pointdistparams);
   glPointSize (10.0);
   glHint (GL_POINT_SMOOTH_HINT, GL_NICEST);
+
+  lp->stardlist = make_starfield (lp->groundRadius);
 
   glClearColor (0.0, 0.0, 0.0, 0.0);
   glClearDepth (1.0);
@@ -340,24 +316,24 @@ release_craters (ModeInfo * mi)
       for (screen = 0; screen < MI_NUM_SCREENS (mi); screen++)
 	{
 	  crater_configuration *lp = &lps[screen];
-	  for (loop = 0; loop < lp->numcraters; loop++)
+	  if ((lp != NULL) && (lp->numcraters != NULL))
 	    {
-	      if (lp->craters[loop] != NULL)
+	      for (loop = 0; loop < lp->numcraters; loop++)
 		{
-		free (lp->craters[loop]);
+		  if (lp->craters[loop] != NULL)
+		    {
+		      free (lp->craters[loop]);
+		    }
 		}
+	      if (lp->craters != NULL) free (lp->craters);
+	      delete_ejemitter (lp->eruption);
 	    }
-	  free (lp->craters);
-	  if (lp->eraser != NULL) free (lp->eraser);
-	  /* segfaults during this call */
-/*       delete_ejemitter(lp->eruption); */
 	}
       free (lps);
       lps = 0;
     }
   FreeAllGL (mi);
 }
-
 
 
 
@@ -385,18 +361,18 @@ draw_craters (ModeInfo * mi)
       glXMakeCurrent (MI_DISPLAY (mi), MI_WINDOW (mi), *(lp->glx_context));
 
       glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
- 
+
       glLoadIdentity ();
-      glCallList(lp->stardlist);
+      glCallList (lp->stardlist);
       glTranslatef (0., 0., -20);
 
 
       /* draw the ground */
-      glPushMatrix();
-	  glRotatef (90., -1., 0., 0.);
-	  glTranslatef(0., 0., lp->groundlevel);
-	glCallList(lp->grounddlist);
-	glPopMatrix();
+      glPushMatrix ();
+      glRotatef (90., -1., 0., 0.);
+      glTranslatef (0., 0., lp->groundlevel);
+      glCallList (lp->grounddlist);
+      glPopMatrix ();
 
       /* loop over the craters */
       for (loop = 0; loop < lp->countcraters; loop++)
@@ -410,20 +386,22 @@ draw_craters (ModeInfo * mi)
 	  glScalef (ccrater->sizefactor, ccrater->sizefactor,
 		    ccrater->sizefactor);
 
-	  if (loop == lp->countcraters - 1) 
+	  if (loop == lp->countcraters - 1)
 	    {
-	      glPushMatrix();
-	      glTranslatef(0., 0., lp->eruptframes * lp->eruptincrement);
-	      	  glCallList(lp->craterdlist);
-		  glPopMatrix();
+	      glPushMatrix ();
+	      glTranslatef (0., 0., lp->eruptframes * lp->eruptincrement);
+	      glCallList (lp->craterdlist);
+	      glPopMatrix ();
 	      if (lp->erupting == true)
 		{
 		  glRotatef (-90., -1., 0., 0.);
-		  lp->erupting = spew_ejecta(lp->eruption);
-		  if (lp->eruptframes > 0) lp->eruptframes--;
+		  lp->erupting = spew_ejecta (lp->eruption);
+		  if (lp->eruptframes > 0)
+		    lp->eruptframes--;
 		}
 	    }
-	  else glCallList(lp->craterdlist);
+	  else
+	    glCallList (lp->craterdlist);
 
 
 	  glPopMatrix ();
@@ -431,11 +409,12 @@ draw_craters (ModeInfo * mi)
 
 	}
 
-      if (lp->erupting == false) lp->ctimer--;
+      if (lp->erupting == false)
+	lp->wtimer--;
 
-      if (lp->ctimer < 0)
+      if (lp->wtimer < 0)
 	{
-	  lp->ctimer = lp->timer;
+	  lp->wtimer = lp->timer;
 	  lp->countcraters++;
 	  if (lp->countcraters > lp->numcraters)
 	    {
@@ -451,9 +430,9 @@ draw_craters (ModeInfo * mi)
 	  else
 	    {
 	      lp->craters[lp->countcraters - 1] = new_crater (lp);
-	      lp->eruptframes = reset_ejemitter(lp->eruption, 0.6);
+	      lp->eruptframes = reset_ejemitter (lp->eruption, 0.6);
 	      lp->erupting = true;
-	      lp->eruptincrement =  -0.18 / (double)lp->eruptframes;
+	      lp->eruptincrement = -0.18 / (double) lp->eruptframes;
 	    }
 	}
 
@@ -469,25 +448,18 @@ draw_craters (ModeInfo * mi)
 
 XSCREENSAVER_MODULE ("CRATERS", craters)
 #endif /* USE_GL */
-
-
   /* -------------------------------------------------------- */
-
 /* set lighting and materials appropriately 
  *
  * put in meteor strike
  *
- * refine options and defaults
- *   step = t parameter for ejecta, also add one for wait between new craters
- *
  * modelview matrix manipulation is inefficient
  *
- * solve ejecta destruction segfault problem
  *
+ * pass in MI for star and crater distribution
  *
  * solve the problem of craters intersecting
  *
  * make shadows for the craters
  *
  */
-
