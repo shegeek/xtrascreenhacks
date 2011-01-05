99d799ac408d98aab627b5258f64262fadee365f
Kelley Nielsen <shegeek-dev@comcast.net>
2011-01-05 07:13:20 -0800

    remote build working, gtk removed from config.ac

--- xtrascreenhacks-0.6/hacks/glx/xbmread.h
+++ xtrascreenhacks-0.6/hacks/glx/xbmread.h
@@ -1,3 +1,5 @@
+#ifndef _XBMREAD_H_
+#define _XBMREAD_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,3 +12,5 @@
 
 int xbmread(char filename[], GLuint rgba, GLuint back_rgba, 
             int *width, int *height, int maxsize, GLuint  rgbapix[]);
+
+#endif
